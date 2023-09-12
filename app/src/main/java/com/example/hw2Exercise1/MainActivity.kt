f0d113267237895dea0684845b29fd51142d5261
texteditor <wh932S@login.missouristate.edu>
2023-09-12 18:03:38 -0500

    HW#2 Exercise 1

--- app/src/main/java/com/example/hw2Exercise1/MainActivity.kt
+++ app/src/main/java/com/example/hw2Exercise1/MainActivity.kt
@@ -1,32 +1,54 @@
-package com.example.hw2Exercise1
+package com.example.hw1withsnackbar
 
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import com.example.hw1withsnackbar.R
+import com.example.hw1withsnackbar.databinding.ActivityMainBinding
+import com.example.hw2Exercise1.question
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
     //Defining both buttons as a 'button' and naming
-    private lateinit var trueButton: Button
-    private lateinit var falseButton: Button
+    //private lateinit var trueButton: Button
+    //private lateinit var falseButton: Button
+
+    //creating a variable for the binding
+    private lateinit var binding : ActivityMainBinding
+
+
+    private val questionBank = listOf(
+        question(R.string.question_australia, true),
+        question(R.string.question_oceans, true),
+        question(R.string.question_mideast, false),
+        question(R.string.question_africa, false),
+        question(R.string.question_americas, true),
+        question(R.string.question_asia, true),
+    )
+
+private var currentIndex = 0
+
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+       // setContentView(R.layout.activity_main)
 
         //Naming and finding both of the buttons
-        trueButton = findViewById(R.id.true_button)
-        falseButton = findViewById(R.id.false_button)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        //trueButton = findViewById(R.id.true_button)
+       // falseButton = findViewById(R.id.false_button)
 
 
 
         //My on onclick listener for the true button
         // and coding it with 'Correct!'
-        trueButton.setOnClickListener {
+        binding.trueButton.setOnClickListener {
 
             val snackBar = Snackbar.make (
                 it,
@@ -46,7 +68,7 @@ class MainActivity : AppCompatActivity() {
 
         //My on onclick listener for the false button
         // and coding it with 'False!'
-        falseButton.setOnClickListener {
+        binding.falseButton.setOnClickListener {
 
             val snackBar = Snackbar.make (
                 it,
@@ -61,5 +83,19 @@ class MainActivity : AppCompatActivity() {
             snackBar.show()
         }
 
+        binding.nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+//            val questionTextResId = questionBank[currentIndex].textResId
+//            binding.questionTextView.setText(questionTextResId)
+                updateQuestion()
+        }
+
+updateQuestion()
+
+    }
+
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        binding.questionTextView.setText(questionTextResId)
     }
 }
---
+++ app/src/main/java/com/example/hw2Exercise1/question.kt
@@ -0,0 +1,6 @@
+package com.example.hw2Exercise1
+
+import androidx.annotation.StringRes
+
+data class question(@StringRes val textResId: Int, val answer: Boolean)
+
